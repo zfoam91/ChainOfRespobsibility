d18882c4bdbb3332809d1fe0998d6365a30fb387
zfoam91 <36771609+zfoam91@users.noreply.github.com>
2022-04-07 11:14:12 +0400

    Add files via upload

---
+++ ChainOfResponsibilityTask/Program/Program.cs
@@ -0,0 +1,10 @@
+﻿using DesignPatterns;
+
+IHandler lower_case = new lowerCase();
+IHandler multiplier = new Multiplier();
+IHandler findemail = new FindEmail();
+
+lower_case.Successor(multiplier);
+multiplier.Successor(findemail); 
+
+lower_case.Handle("ASJDASjskdas121312 AJKDjad 12131 jdajdjd@djs.com 31912 dsadfksdaf@kkfasd.com");
